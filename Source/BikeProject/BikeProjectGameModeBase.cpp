a8e703c4967952678dde44e1c03311133a0aa4fe
tavi <octavial@gmx.com>
2021-01-27 17:00:39 +0000

    Added Bike Character

--- Source/BikeProject/BikeProjectGameModeBase.cpp
+++ Source/BikeProject/BikeProjectGameModeBase.cpp
@@ -3,3 +3,32 @@
 
 #include "BikeProjectGameModeBase.h"
 
+void ABikeProjectGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	check(GEngine != nullptr);
+
+	// Display a debug message for five seconds. 
+	// The -1 "Key" value argument prevents the message from being updated or refreshed.
+	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, TEXT("Hello World, this is Bike Project!"));
+
+	ChangeMenuWidget(StartingWidgetClass);
+}
+
+void ABikeProjectGameModeBase::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+    if (CurrentWidget != nullptr)
+    {
+        CurrentWidget->RemoveFromViewport();
+        CurrentWidget = nullptr;
+    }
+    if (NewWidgetClass != nullptr)
+    {
+        CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+        if (CurrentWidget != nullptr)
+        {
+            CurrentWidget->AddToViewport();
+        }
+    }
+}
--- Source/BikeProject/BikeProjectGameModeBase.h
+++ Source/BikeProject/BikeProjectGameModeBase.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
 #include "GameFramework/GameModeBase.h"
 #include "BikeProjectGameModeBase.generated.h"
 
@@ -14,4 +15,19 @@ class BIKEPROJECT_API ABikeProjectGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+    /** Remove the current menu widget and create a new one from the specified class, if provided. */
+    UFUNCTION(BlueprintCallable, Category = "UMG Game")
+        void ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass);
+
+protected:
+    virtual void StartPlay() override;
+
+    /** The widget class we will use as our menu when the game starts. */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UMG Game")
+    TSubclassOf<UUserWidget> StartingWidgetClass;
+
+    /** The widget instance that we are using as our menu. */
+    UPROPERTY()
+    UUserWidget* CurrentWidget;
 };
